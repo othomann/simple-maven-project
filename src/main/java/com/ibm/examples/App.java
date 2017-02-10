2e780793e6e1bc6f7511c97bc5ff1e7e6ae5f50a
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2017-02-09 20:00:27 -0500

    set type to snapshot
    Signed-off-by: Olivier Thomann <Olivier_Thomann@ca.ibm.com>

--- src/main/java/com/ibm/examples/App.java
+++ src/main/java/com/ibm/examples/App.java
@@ -5,6 +5,6 @@ package com.ibm.examples;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("This is  simple Hello World application");
 	}
 }
